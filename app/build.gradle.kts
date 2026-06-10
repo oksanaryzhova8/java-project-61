83bea60d9140f1373bc4fe2ee4978cd512ebcf1a
Oksana <oksanaryzhova@mail.ru>
2026-06-10 06:12:59 +0200

    Add SonarQube analysis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     application
     id("com.github.ben-manes.versions") version "0.54.0"
+    id("org.sonarqube") version "7.3.1.8318"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "oksanaryzhova8_java-project-61")
+        property("sonar.organization", "oksanaryzhova8")
+    }
 }
 
 group = "hexlet.code"
