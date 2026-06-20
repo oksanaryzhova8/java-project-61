47a76e7f1071ce01cf3c41973b19baa739fe092e
Oksana <oksanaryzhova@mail.ru>
2026-06-21 01:41:13 +0200

    Add Even game and screenshots

--- README.md
+++ README.md
@@ -7,4 +7,8 @@
 
 [![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=oksanaryzhova8_java-project-61&metric=code_smells)](https://sonarcloud.io/summary/new_code?id=oksanaryzhova8_java-project-61)
 
-[![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=oksanaryzhova8_java-project-61&metric=duplicated_lines_density)](https://sonarcloud.io/summary/new_code?id=oksanaryzhova8_java-project-61)
+[![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=oksanaryzhova8_java-project-61&metric=duplicated_lines_density)](https://sonarcloud.io/summary/new_code?id=oksanaryzhova8_java-project-61)## Even game demonstration
+### Victory
+![Even game victory](images/even-win.png)
+### Defeat
+![Even game defeat](images/even-lose.png)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
 
-        Cli.greet();
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        System.out.println("You choice: " + choice);
+
+        switch (choice) {
+            case 1:
+                Cli.greet();
+                break;
+
+            case 2:
+                Even.parityCheck();
+                break;
+
+            case 0:
+                break;
+            default:
+                System.out.println("Unknown game number.");
+        }
     }
 }
